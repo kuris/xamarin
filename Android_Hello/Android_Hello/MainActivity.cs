925c1f2e9488a92d309c7fb5f9b1ae85ddf2a1e0
fitnesskimybkim <fitnesskim@naver.com>
2018-10-31 14:35:53 +0900

    call history add

--- Android_Hello/Android_Hello/MainActivity.cs
+++ Android_Hello/Android_Hello/MainActivity.cs
@@ -4,16 +4,31 @@ using Android.Runtime;
 using Android.Support.V7.App;
 using Android.Widget;
 using Android.Content;
+using System.Collections.Generic;
+
 namespace Android_Hello
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+            callHistoryButton.Click += (sender, e) =>
+            {
+                
+                var intent = new Intent(this,
+                typeof(CallHistoryActivity));
+               
+                 intent.PutStringArrayListExtra("phone_numbers",
+                phoneNumbers);
+                StartActivity(intent);
+            };
+
             EditText phoneNumberText =
                FindViewById<EditText>(Resource.Id.PhoneNumberText);
             //전화걸기 버튼
@@ -26,7 +41,8 @@ namespace Android_Hello
                     callButton.Enabled = true;
                 else
                     callButton.Enabled = false;
-            };            callButton.Click += (object sender, System.EventArgs e) =>
+            };
+            callButton.Click += (object sender, System.EventArgs e) =>
             {
                 //Make a Call 버튼 클릭시 전화를 건다.
                 var callDialog = new Android.App.AlertDialog.Builder(this);
@@ -35,6 +51,9 @@ namespace Android_Hello
                 // 전화걸기 위한 인텐트 생성
                 callDialog.SetNeutralButton("Call", delegate
                 {
+                    phoneNumbers.Add(phoneNumberText.Text);
+                    callHistoryButton.Enabled = true;
+               
                     // 인텐트는 액티비티의 전환이 일어날 때 호출하거나 메시지를 전달하는 매개체
                     // 암시적 인텐트 : 전환될 곳을 직접 지정하지 않고 액션을 적어서 사용한다.
                     // 명시적 인텐트 : 전환될 액티비티를 직접 적어서 표현하는 방법을 사용한다.
