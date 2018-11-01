c014d23073d25fa7a9afb6ef7bb49f258112fb8d
fitnesskimybkim <fitnesskim@naver.com>
2018-11-01 11:39:46 +0900

    xamarin forms call history

---
+++ Hello_iOS/Hello_iOS/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace Hello_iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            PhoneNumberText.ResignFirstResponder();
+            CallButton.TouchUpInside += (object sender, EventArgs
+                   e) =>
+              {
+                  var url = new NSUrl("tel:" + PhoneNumberText.Text);
+                  if(!UIApplication.SharedApplication.OpenUrl(url))
+                  {
+                      var alert = UIAlertController.Create("Not Supported", "Schema 'tel:' is not supported on this device ", UIAlertControllerStyle.Alert);
+                      alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                      PresentViewController(alert, true, null);
+                  }
+
+
+
+                  // Perform any additional setup after loading the view, typically from a nib.
+              };
+            
+    }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ UseSayHelloLib/UseSayHelloLib/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Com.Example.Topcredu.Helloaar;
+using Com.Example.Topcredu.Helloarr;
+
+namespace UseSayHelloLib
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ISayHello hello;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
+            hello = new SayHelloImpl();
+            button1.Click += (sender, args) => textView1.Text = hello.SayHello("테스트");
+          
+     
+        }
+    }
+}
